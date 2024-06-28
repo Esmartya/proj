135ba756de596c5040f6acb92fee509b44d70152
Esmartya <esmartianova@gmail.com>
2024-06-28 21:16:50 +0300

    изменена логика main

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,10 +2,11 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println(------);
         System.out.println("Hello World!");
-        System.out.println("11111111");
+        System.out.println("11100011");
         System.out.println("11111122");
-        System.out.println("11111111");
-        System.out.println("11111111");
+        System.out.println("1110001111");
+        System.out.println("11111001");
     }
 }
